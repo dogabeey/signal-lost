788b7fba878cdd5ad3df74b429def0d5b1e66d2c
Doğa <dogabeey@gmail.com>
2026-09-17 20:12:27 +0300

    android and admob integration

---
+++ android/app/src/main/java/com/dogabeey/asteroidbelt/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dogabeey.asteroidbelt;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/build_info.js
+++ src/build_info.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
   version: '0.9.9',
-  number: '1133',
-  date: 'September 6, 2026',
+  number: '1135',
+  date: 'September 17, 2026',
   label: 'EARLY ACCESS',
 }
